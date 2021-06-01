856257a8908534da0b8f797a5c36fddabab629ea
Anastasia Churikova <sattarova-91@inbox.ru>
2021-06-01 21:30:39 +0100

    Add tool configs

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'no-useless-escape': 'off',
+  },
+};
